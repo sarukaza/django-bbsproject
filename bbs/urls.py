6cf9ed4cbe0a09fd435c12d58a0016cc325bf4c8
kazato <k.sarudate.sys24@morijyobi.ac.jp>
2025-06-06 14:35:03 +0900

    first commit

---
+++ bbs/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'bbs'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'), #一覧ページのビュー
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), #投稿詳細ページのビュー
+    path('create/', views.CreateView.as_view(), name='create'), #新規投稿ページのビュー
+    path('<int:pk>/update/', views.UpdateView.as_view(), name='update'), #投稿編集ページのビュー
+    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'), #投稿削除ページのビュー
+    path('search/', views.search, name='search'),    # 検索
+
+]
