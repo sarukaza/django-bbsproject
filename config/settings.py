bf77038195aaa848ae177ce1c93e141b8bc9749e
kazato <k.sarudate.sys24@morijyobi.ac.jp>
2025-06-09 14:44:33 +0900

    third commit

--- config/settings.py
+++ config/settings.py
@@ -16,12 +16,11 @@ import environ
 from decouple import config
 from dj_database_url import parse as dburl
 
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, ".env"))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
